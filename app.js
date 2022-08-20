71b693932c402a20cc49d1a9ee68ccc4d05dd952
elnur004 <lnur004@gmail.com>
2022-08-21 02:07:41 +0400

    added computer choice logic

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const startGameBtn = document.getElementById('start-game-btn');
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
-
 const DEFAULT_USER_CHOICE = ROCK;
+
+const RESULT_DRAW = 'DRAW';
+const RESULT_PAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+
 let gameIsRunning = false;
 
 const getPlayerChoice = function () {
@@ -20,10 +24,40 @@ const getPlayerChoice = function () {
   return selection;
 };
 
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (computerChoice, playerChoice) {
+  if (computerChoice === playerChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (computerChoice === ROCK && playerChoice === PAPER) ||
+    (computerChoice === PAPER && playerChoice === SCISSORS) ||
+    (computerChoice === SCISSORS && playerChoice === ROCK)
+  ) {
+    return RESULT_PAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+    {
+    }
+  }
+};
+
 startGameBtn.addEventListener('click', function () {
   if (gameIsRunning) return;
   gameIsRunning = false;
   console.log('Game is starting...');
   const playerChoice = getPlayerChoice();
-  console.log(playerChoice);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(computerChoice);
+  console.log(winner);
 });
